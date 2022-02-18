878401e1cfe4651dc2bfcafdecc0430176eb63bb
Cosme Escobedo <comeoes@gmail.com>
2022-02-17 20:51:40 -0700

    importer command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\RunImporters;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command(RunImporters::class)->daily()->at('02:00')->timezone('America/Los_Angeles');
     }
 
     /**
