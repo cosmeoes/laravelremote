fc586e800b454eed78ff6ca2b1f863e2d6aecd5e
Cosme Escobedo <comeoes@gmail.com>
2022-02-27 13:11:42 -0700

    remove overflow scroll

--- resources/views/livewire/feedback-form.blade.php
+++ resources/views/livewire/feedback-form.blade.php
@@ -6,7 +6,7 @@
             <svg x-show="open" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </div>
     </div>
-    <form x-show="open" wire:submit.prevent="submit" class="top-0 flex flex-col flex-1 overflow-scroll space-y-2">
+    <form x-show="open" wire:submit.prevent="submit" class="flex flex-col flex-1 space-y-2">
         @if(session()->has('message')) 
             <p class="px-2 text-base text-center text-white bg-green-500 rounded" x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)">
                 {{session('message')}}
