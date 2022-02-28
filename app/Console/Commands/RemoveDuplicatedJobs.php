62906458ba37eb308776473b5c0c0cdc0a82cf02
Cosme Escobedo <comeoes@gmail.com>
2022-02-27 18:06:20 -0700

    remove duplicated jobs

---
+++ app/Console/Commands/RemoveDuplicatedJobs.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\JobPost;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class RemoveDuplicatedJobs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lr:remove-duplicated';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Removes duplicated jobs by position and company';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $duplicated = $this->getDuplicated();
+
+        $removed = 0;
+        foreach($duplicated as $d) {
+            $removed += $this->onlyOne($d);
+        }
+
+        Log::info("Removed $removed duplicated jobs");
+        return 0;
+    }
+
+    public function getDuplicated()
+    {
+        return JobPost::selectRaw('count(*) count, position, company')
+            ->groupBy('position', 'company')
+            ->having('count', '>=', 2)
+            ->get();
+    }
+
+    public function onlyOne($duplicated)
+    {
+        $jobs = JobPost::query()->where('position', $duplicated->position)->where('company', $duplicated->company)->get();
+
+        $deleted = 0;
+        $jobs->skip(1)->each(function ($job) use (&$deleted) {
+            $deleted++;
+            $job->delete();
+        });
+
+        return $deleted;
+    }
+}
--- app/Models/JobPost.php
+++ app/Models/JobPost.php
@@ -20,9 +20,18 @@ class JobPost extends Model
     public static function storeFromSource($jobs)
     {
         foreach ($jobs as $job) {
-            if (JobPost::where('source_url', $job['source_url'])->orWhere('apply_url', $job['apply_url'])->doesntExist()) {
+            if (self::shouldAddJob($job)) {
                 self::create($job);
             }
         }
+    }
+
+    public static function shouldAddJob($job)
+    {
+        return JobPost::where('source_url', $job['source_url'])->orWhere('apply_url', $job['apply_url'])->orWhere(function($query) use ($job) {
+            $query->where('position', $job->position)
+                  ->where('company', $job->company)
+                  ->where('created_at', '>=', now()->addWeek());
+        })->doesntExist();
     }
 }
