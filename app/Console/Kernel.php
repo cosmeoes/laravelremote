ceac99f62e1572ef8a5e2ee12048cadc8ca015c0
Cosme Escobedo <comeoes@gmail.com>
2022-03-03 08:33:02 -0700

    fix scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Console\Commands\RunImporters;
 use App\Console\Commands\SendJobAlert;
+use App\Console\Commands\TagNewPosts;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
